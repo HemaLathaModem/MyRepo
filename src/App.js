50df31c9d7114ba9b976ffbd4b76599190471b7c
HemaLatha <hema.modem@capgemini.com>
2020-04-14 10:09:07 +0530

    Hello World Applcation

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import myfirst from './components/myfirst'
+import Hello from './components/Hello'
+
+function App() {
+  return (
+    <div className="App">
+      <Hello/>
+      <myfirst/>
+    </div>
+  );
+}
+
+export default App;
